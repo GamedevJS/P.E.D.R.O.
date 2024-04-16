fb2a43a7728f0df9762a348f357292aedf33c28c
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2024-04-15 23:45:41 -0300

    implementa inimigo que segue

---
+++ main/enemy.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+var SPEED = 2500.0
+var DIR : Vector2 = Vector2.ZERO
+var ATTACKING : bool = false
+var IS_ALIVE = true
+var CHASE = false
+var PLAYER : CharacterBody2D = null
+
+@onready var animation := $Player as AnimationPlayer
+@onready var sprites := $Sprites as Sprite2D
+@onready var detection_area := $DetectionArea as Area2D
+
+
+func _physics_process(delta):
+	if CHASE:
+		DIR = (PLAYER.position - position).normalized()
+		velocity = DIR * SPEED * delta
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
+func _process(delta):
+	animation_handler()
+
+func animation_handler():
+	if DIR.x > 0:
+		sprites.scale.x = 1
+	elif DIR.x < 0:
+		sprites.scale.x = -1
+	if ATTACKING == false:
+		if velocity != Vector2.ZERO:
+			animation.play("running")
+		else:
+			animation.play("idle")
+
+
+func _on_detection_area_body_entered(body):
+	PLAYER = body
+	CHASE = true
+
+
+func _on_detection_area_body_exited(body):
+	PLAYER = null
+	CHASE = false
--- main/scripts/player.gd
+++ main/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var SPEED = 5000.0
 var DIR : Vector2 = Vector2.ZERO
 var ATTACKING : bool = false
+var IS_ALIVE = true
 
 @onready var animation := $Player as AnimationPlayer
 @onready var sprites := $Sprites as Sprite2D
@@ -33,8 +34,13 @@ func animation_handler():
 		SPEED = 1000.0
 		if DIR.y == -1:
 			animation.play("attack1_up")
+		elif DIR.y == 1:
+			animation.play("attack1_down")
 		else:
 			animation.play("attack1_side")
+	if Input.is_action_just_pressed("attack2"):
+		IS_ALIVE = false
+		animation.play("die")
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name.contains("attack1"):
