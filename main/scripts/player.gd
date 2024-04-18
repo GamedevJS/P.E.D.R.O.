aaa77593b2464e62382a5b51bec155e0e74e5fec
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2024-04-18 11:37:53 -0300

    melhoras nas animações e organização
    - agora o personagem ataca para onde o player clicar
    - organização do projeto

--- main/scripts/player.gd
+++ main/scripts/player.gd
@@ -14,6 +14,8 @@ var HEALTH;
 var INVINCILITY = false
 var UNDER_ATTACK : bool = false
 
+var SEN_45 = pow(2, 1/2)/2
+
 @onready var animation := $Player as AnimationPlayer
 @onready var sprites := $Sprites as Sprite2D
 @onready var hit_cooldown_timer := $HitCooldown as Timer
@@ -58,14 +60,39 @@ func animation_handler():
 	if Input.is_action_just_pressed("attack1"):
 		ATTACKING = true
 		SPEED = SPEED * 0.1
-		if DIR.y == -1:
+		var attack_direction = handle_attack_direction()
+		if attack_direction == 1:
 			animation.play("attack1_up")
-		elif DIR.y == 1:
-			animation.play("attack1_down")
-		else:
+		elif attack_direction == 2:
 			animation.play("attack1_side")
+			sprites.scale.x = -1
+		elif attack_direction == 3:
+			animation.play("attack1_side")
+			sprites.scale.x = 1
+		elif attack_direction == 4:
+			animation.play("attack1_down")
+		
+			
+		#if DIR.y == -1:
+			#animation.play("attack1_up")
+		#elif DIR.y == 1:
+			#animation.play("attack1_down")
+		#else:
+			#animation.play("attack1_side")
 
+func handle_attack_direction():
+	var attack_dir = get_local_mouse_position() - position
+	if attack_dir.y < SEN_45 * attack_dir.x and attack_dir.y < -SEN_45 * attack_dir.x:
+		return 1
+	elif attack_dir.y >= SEN_45 * attack_dir.x and attack_dir.y < -SEN_45 * attack_dir.x:
+		return 2
+	elif attack_dir.y < SEN_45 * attack_dir.x and attack_dir.y >= -SEN_45 * attack_dir.x:
+		return 3
+	elif attack_dir.y >= SEN_45 * attack_dir.x and attack_dir.y >= -SEN_45 * attack_dir.x:
+		return 4
+	return 0
 
+	
 # Signals
 
 func _on_animation_player_animation_finished(anim_name):
@@ -73,3 +100,13 @@ func _on_animation_player_animation_finished(anim_name):
 		ATTACKING = false
 		SPEED = STATS.speed
 
+func recieve_damage(damage: int):
+	if !INVINCILITY:
+		HEALTH -= damage
+		INVINCILITY = true
+		print(HEALTH)
+		hit_cooldown_timer.start()
+	
+
+func _on_hit_cooldown_timeout():
+	INVINCILITY = false
